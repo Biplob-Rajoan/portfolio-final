70f5643e0bb23f75b4b11a62aa0e40fe91a40626
Biplob-Rajoan <biplobrajoan2089@gmail.com>
2024-11-29 20:33:49 +0600

    restuarent site added portfolio

--- src/content_option.js
+++ src/content_option.js
@@ -1,24 +1,26 @@
 const logotext = "RAJOAN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Rajoan Bosunia",
+  description:
+    "I’m Rajoan Bosunia Full stack devloper,currently working in Dhaka",
 };
 
 const introdata = {
-    title: "I’m Rajoan",
-    animated: {
-        first: "I love coding",
-        second: "Building fast and accessible web experiences.",
-        third: "Turning ideas into interactive user interfaces.",
-
-    },
-    description: "Passionate about turning ideas into web applications with responsive designs and a seamless user experience.",
-    your_img_url: "https://i.ibb.co.com/pjG2mdK/6208502920496923147.jpg",
+  title: "I’m Rajoan",
+  animated: {
+    first: "I love coding",
+    second: "Building fast and accessible web experiences.",
+    third: "Turning ideas into interactive user interfaces.",
+  },
+  description:
+    "Passionate about turning ideas into web applications with responsive designs and a seamless user experience.",
+  your_img_url: "https://i.ibb.co.com/pjG2mdK/6208502920496923147.jpg",
 };
 
 const dataabout = {
-    title: "About MySelf",
-    aboutme: "I am a passionate front-end developer skilled in crafting responsive, user-friendly web applications. Proficient in modern tools, clean code, problem-solving, and innovative designs to deliver seamless, dynamic, and engaging user experiences efficiently.",
+  title: "About MySelf",
+  aboutme:
+    "I am a passionate front-end developer skilled in crafting responsive, user-friendly web applications. Proficient in modern tools, clean code, problem-solving, and innovative designs to deliver seamless, dynamic, and engaging user experiences efficiently.",
 };
 // const worktimeline = [{
 //     jobtitle: "Designer of week",
@@ -38,168 +40,187 @@ const dataabout = {
 // ];
 
 const skills = [
-    {
-        name: "HTML",
-        value: 85,
-    },
-    {
-        name: "CSS",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 85,
-    },
-    {
-        name: "Next.Js",
-        value: 85,
-    },
-    {
-        name: "Typescript",
-        value: 85,
-    },
-    {
-        name: "Node",
-        value: 85,
-    },
-    {
-        name: "Express.Js",
-        value: 85,
-    },
-    {
-        name: "MongoDB",
-        value: 85,
-    },
-    {
-        name: "PostMan",
-        value: 85,
-    },
+  {
+    name: "HTML",
+    value: 85,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 85,
+  },
+  {
+    name: "Next.Js",
+    value: 85,
+  },
+  {
+    name: "Typescript",
+    value: 85,
+  },
+  {
+    name: "Node",
+    value: 85,
+  },
+  {
+    name: "Express.Js",
+    value: 85,
+  },
+  {
+    name: "MongoDB",
+    value: 85,
+  },
+  {
+    name: "PostMan",
+    value: 85,
+  },
 ];
 
-const services = [{
+const services = [
+  {
     title: "Basic Website Development",
-    description: "Creating simple and functional websites using HTML, CSS, and JavaScript tailored to your business or personal needs.",
-},
-{
+    description:
+      "Creating simple and functional websites using HTML, CSS, and JavaScript tailored to your business or personal needs.",
+  },
+  {
     title: "Landing Page Development",
-    description: "Creating visually appealing landing pages optimized for conversions and lead generation.",
-},
-{
+    description:
+      "Creating visually appealing landing pages optimized for conversions and lead generation.",
+  },
+  {
     title: "Portfolio Websites",
-    description: "Designing clean and modern portfolio websites for individuals or businesses to showcase their work.",
-},
-{
+    description:
+      "Designing clean and modern portfolio websites for individuals or businesses to showcase their work.",
+  },
+  {
     title: "Template Customization",
-    description: "Modifying and customizing website templates to align with your brand and preferences.",
-},
-{
+    description:
+      "Modifying and customizing website templates to align with your brand and preferences.",
+  },
+  {
     title: "Responsive Design",
-    description: "Building websites that adapt seamlessly to mobile, tablet, and desktop devices for a consistent user experience..",
-},
-{
+    description:
+      "Building websites that adapt seamlessly to mobile, tablet, and desktop devices for a consistent user experience..",
+  },
+  {
     title: "Bug Fixes and Updates",
-    description: "Fixing layout issues, broken links, or other minor bugs, and updating content as required.",
-},
+    description:
+      "Fixing layout issues, broken links, or other minor bugs, and updating content as required.",
+  },
 ];
 
-const dataportfolio = [{
-    img: "https://picsum.photos/400/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/800/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
+const dataportfolio = [
+  {
+    img: "https://i.ibb.co.com/NtTyL1v/final1.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://bistro-boss-90103.web.app/",
+  },
+  {
+    img: "https://i.ibb.co.com/gTD2WsW/final2.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://bistro-boss-90103.web.app/",
+  },
+  {
+    img: "https://i.ibb.co.com/gTD2WsW/final2.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://bistro-boss-90103.web.app/",
+  },
+  {
     img: "https://picsum.photos/400/600/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
     link: "#",
-},
-{
-    img: "https://picsum.photos/400/300/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
+  },
+  {
+    img: "https://i.ibb.co.com/LZ6rrS4/final3.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
     link: "#",
-},
-{
+  },
+  {
     img: "https://picsum.photos/400/700/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
     link: "#",
-},
+  },
 
-{
-    img: "https://picsum.photos/400/600/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/300/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/550/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/700/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
+  // {
+  //     img: "https://picsum.photos/400/600/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/300/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/550/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/700/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "mdrajoanbosunya@gmail.com",
-    YOUR_FONE: "+8801737542089",
-    description: //"Creative front-end developer passionate about building responsive, user-friendly web applications with clean code, modern tools, and innovative designs.Feel free to reach out anytime!" ,
-        <div>
-            <p>Creative front-end developer building responsive applications with clean code, modern tools, and innovative designs.</p>
-            <p>Feel free to reach me out anytime!</p>
-        </div>,
+  YOUR_EMAIL: "mdrajoanbosunya@gmail.com",
+  YOUR_FONE: "+8801737542089",
+  //"Creative front-end developer passionate about building responsive, user-friendly web applications with clean code, modern tools, and innovative designs.Feel free to reach out anytime!" ,
+  description: (
+    <div>
+      <p>
+        Creative front-end developer building responsive applications with clean
+        code, modern tools, and innovative designs.
+      </p>
+      <p>Feel free to reach me out anytime!</p>
+    </div>
+  ),
 
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_fvhbrx9",
-    YOUR_TEMPLATE_ID: "template_h1egjaf",
-    YOUR_USER_ID: "wCBgDVVCsJMiY9Bhz",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_fvhbrx9",
+  YOUR_TEMPLATE_ID: "template_h1egjaf",
+  YOUR_USER_ID: "wCBgDVVCsJMiY9Bhz",
 };
 
 const socialprofils = {
-    github: "https://github.com/Biplob-Rajoan",
-    facebook: "https://www.facebook.com/rajoan.bosunya",
-    linkedin: "https://www.linkedin.com/in/md-rajoan-bosunya-151a44253/",
-    twitter: "https://twitter.com",
+  github: "https://github.com/Biplob-Rajoan",
+  facebook: "https://www.facebook.com/rajoan.bosunya",
+  linkedin: "https://www.linkedin.com/in/md-rajoan-bosunya-151a44253/",
+  twitter: "https://twitter.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-   // worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  // worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
